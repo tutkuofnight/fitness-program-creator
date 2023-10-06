3a6d11854d702babadd52ef2c1cca9be5ce1f0f9
Tutku Uçan <mettheus0@gmail.com>
2023-10-06 09:40:51 +0300

    Add constants

---
+++ app/utils/contants.js
@@ -0,0 +1,22 @@
+export const program = {
+  name: "",
+  days: [],
+  private: false,
+  collections: []
+}
+export const collection = {
+  id: 1,
+  name: "",
+  items: []
+}
+export const workoutProgramItem = {
+  id: 1,
+  name: "",
+  set: "",
+  count: ""
+}
+export const dietProgramItem = {
+  id: 1,
+  name: "",
+  type: ["gr." , "number"]
+}
